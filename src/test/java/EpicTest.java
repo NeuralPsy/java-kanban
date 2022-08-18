6a5bb46ea5532fdb38bebec9df4257b556285166
NeuralPsy <kirill.neuralpsy@gmail.com>
2022-08-18 21:22:30 +0600

    Rename addNewApic test to shouldAddNewEpicNormalEmptyNoSubtasks. Now it is able to check epic status

--- src/test/java/EpicTest.java
+++ src/test/java/EpicTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import taskmanager.Manager.InMemoryTasksManager;
 import taskmanager.Manager.TaskManager;
 import taskmanager.TaskTypes.Epic;
-
+import taskmanager.TaskTypes.TaskStatus;
 
 
 import java.util.ArrayList;
@@ -14,7 +14,7 @@ public class EpicTest {
 
 
     @Test
-    public void addNewEpic() {
+    void shouldAddNewEpicNormalEmptyNoSubtasks() {
         TaskManager taskManager = new InMemoryTasksManager();
         final Epic epic = new Epic("Эпик без названия", 0, "Вот оно! Настоящее описание эпика");
         final int epicId = taskManager.addEpic(epic);
@@ -23,12 +23,13 @@ public class EpicTest {
         assertNotNull(savedEpic, "Эпик не создан. Список эпиков пуст.");
         assertEquals(epic, savedEpic, "Эпики не совпадают.");
         final ArrayList<Integer> epics = taskManager.getEpicsList();
+        final ArrayList<Integer> subtasks = taskManager.getSubTasksList();
 
-        assertNotNull(epics, "Задачи на возвращаются.");
-        assertEquals(1, epics.size(), "Количество эпиков неверное");
-        assertEquals(epic, savedEpic, "Задачи не совпадают.");
+        assertNotNull(epics, "Эпик не попал в список эпиков.");
+        assertEquals(1, epics.size(), "Количество эпиков в списке неверное");
+        assertEquals(epic, savedEpic, "Эпики не совпадают.");
+        assertEquals(TaskStatus.NEW, savedEpic.getStatus(), "В новом эпике без подзадач должен быть статус 'NEW' ");
 
     }
-
 
 }

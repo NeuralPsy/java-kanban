0c9652622060cb6d6a410660b09dec07734edbe1
NeuralPsy <kirill.neuralpsy@gmail.com>
2022-08-21 00:10:58 +0600

    Add HistoryManagerTest class and all of HistoryManager interface methods tests

--- src/test/java/taskmanager/Manager/HistoryManagerTest.java
+++ src/test/java/taskmanager/Manager/HistoryManagerTest.java
@@ -3,6 +3,7 @@ package taskmanager.Manager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import taskmanager.TaskTypes.Epic;
+import taskmanager.TaskTypes.Subtask;
 import taskmanager.TaskTypes.Task;
 
 import java.util.ArrayList;
@@ -13,8 +14,23 @@ class HistoryManagerTest {
 
     HistoryManager historyManager = new InMemoryHistoryManager();
 
-    @BeforeEach
     void setUp() {
+        Task task = new Task("Задача", 0, "Без названия");
+        Task task2 = new Task("Задача", 1, "Без названия");
+        Task task3 = new Task("Задача", 2, "Без названия");
+        Task task4 = new Task("Задача", 3, "Без названия");
+        Epic epic1 = new Epic("Задача", 4,"Без названия");
+        Epic epic2 = new Epic("Задача", 5, "Без названия");
+        Subtask subtask1 = new Subtask("Подзадача", 6, "Без названия", 4);
+        Subtask subtask2 = new Subtask("Подзадача", 7, "Без названия", 5);
+        historyManager.add(task);
+        historyManager.add(task2);
+        historyManager.add(task3);
+        historyManager.add(task4);
+        historyManager.add(epic1);
+        historyManager.add(epic2);
+        historyManager.add(subtask1);
+        historyManager.add(subtask2);
     }
 
     @Test
@@ -32,28 +48,28 @@ class HistoryManagerTest {
 
     @Test
     void getHistory() {
-        Task task = new Task("Задача", 0, "Без названия");
-        Task task2 = new Task("Задача", 1, "Без названия");
-        Task task3 = new Task("Задача", 2, "Без названия");
-        Task task4 = new Task("Задача", 3, "Без названия");
-        Epic epic1 = new Epic("Задача", 4,"Без названия");
-        Epic epic2 = new Epic("Задача", 5, "Без названия");
-        historyManager.add(task);
-        historyManager.add(task2);
-        historyManager.add(task3);
-        historyManager.add(task4);
-        historyManager.add(epic1);
-        historyManager.add(epic2);
+        setUp();
 
         ArrayList<Integer> history = historyManager.getHistory();
-        assertEquals(6, history.size());
+        assertEquals(8, history.size());
 
-        assertEquals("[0, 1, 2, 3, 4, 5]", history.toString());
+        assertEquals("[0, 1, 2, 3, 4, 5, 6, 7]", history.toString());
 
     }
 
     @Test
     void remove() {
+        Task task = new Task("Задача", 0, "Без названия");
+        Task task2 = new Task("Задача", 1, "Без названия");
+        Task task3 = new Task("Задача", 2, "Без названия");
+        historyManager.add(task);
+        historyManager.add(task2);
+        historyManager.add(task3);
+        assertEquals(3, historyManager.getHistory().size());
+        historyManager.remove(task2);
+        assertEquals(2, historyManager.getHistory().size());
+
+
     }
 
     @Test
@@ -64,19 +80,25 @@ class HistoryManagerTest {
         Task task4 = new Task("Задача", 3, "Без названия");
         Epic epic1 = new Epic("Задача", 4,"Без названия");
         Epic epic2 = new Epic("Задача", 5, "Без названия");
+        Subtask subtask1 = new Subtask("Подзадача", 6, "Без названия", 4);
+        Subtask subtask2 = new Subtask("Подзадача", 7, "Без названия", 5);
         historyManager.add(task);
         historyManager.add(task2);
         historyManager.add(task3);
         historyManager.add(task4);
         historyManager.add(epic1);
         historyManager.add(epic2);
+        historyManager.add(subtask1);
+        historyManager.add(subtask2);
 
+        ArrayList<Task> history = historyManager.getTasks();
         assertEquals(task, historyManager.getTasks().get(0));
         assertEquals(task2, historyManager.getTasks().get(1));
         assertEquals(task3, historyManager.getTasks().get(2));
         assertEquals(task4, historyManager.getTasks().get(3));
         assertEquals(epic1, historyManager.getTasks().get(4));
         assertEquals(epic2, historyManager.getTasks().get(5));
+        assertEquals(8, history.size());
     }
 
     @Test
